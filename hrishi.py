fbf2662abf994b31e4c270f4d61776943cad9341
Hrishicoderr <hrishikeshdeshmukh11a@gmail.com>
2026-02-06 14:00:37 +0530

    Add files via upload

---
+++ hrishi.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  6 13:04:46 2026
+
+@author: User
+"""
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+
+print("\n..... Student Information.....")
+
+print("Name:", name)
+print("Age:", age)
